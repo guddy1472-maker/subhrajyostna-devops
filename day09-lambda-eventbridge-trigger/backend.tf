4f05f519571392f4517712d44dace398ed7465cd
subhrajyostna <142746453+codeNinja-2023@users.noreply.github.com>
2025-11-13 03:58:37 +0530

    update

---
+++ day09-lambda-eventbridge-trigger/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-statefile-bucket-29thoct"
+    key    = "day09/lambda-eventbridge/terraform.tfstate"
+    #use_lockfile = true # enabling state locking in latest terraform versions
+    region = "us-east-1"
+    #
+  }
+}
---
+++ day09-lambda-eventbridge-trigger/lambdafunction.zip
@@ -0,0 +1,6 @@
+def lambda_handler(event, context):
+    print("✅ Lambda executed successfully (EventBridge Trigger)")
+    return {
+        "statusCode": 200,
+        "message": "Lambda executed via EventBridge schedule!"
+    }
