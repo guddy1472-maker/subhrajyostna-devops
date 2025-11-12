546e1efe4538b6bc819336907c9b86ffb7cef135
subhrajyostna <142746453+codeNinja-2023@users.noreply.github.com>
2025-11-13 04:24:19 +0530

    update

--- day05-lifecyclerules/main.tf
+++ day05-lifecyclerules/main.tf
@@ -1,18 +1,21 @@
-resource "aws_instance" "dev" {
-    ami = "ami-07860a2d7eb515d9ab"
-    instance_type = "t2.micro"
+resource "aws_instance" "name" {
+  # ami = "ami-07860a2d7eb515d9a"
+    ami = "ami-0bdd88bd06d16ba03"
+    instance_type = "t3.micro"
     tags = {
-      Name = "dev"
+      Name = "lifecycle-ec2"
     }
+    lifecycle {
+     create_before_destroy = true
+    }
+    #lifecycle {
+    #  prevent_destroy = true
+    #}
+    # lifecycle {
+    #   ignore_changes = [tags]
+    # }
 
-    # lifecycle {
-    #   create_before_destroy = true
-    # }
-    # lifecycle {
-    #   ignore_changes = [tags,  ]
-    # }
-    # lifecycle {
-    #   prevent_destroy = true
-    # }
-  
+    
+
 }
+
