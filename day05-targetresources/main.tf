f6c14aeed49a3dcec9268bccf7f94e14e0b8a6ae
subhrajyostna <142746453+codeNinja-2023@users.noreply.github.com>
2025-11-13 04:21:43 +0530

    update

--- day05-targetresources/main.tf
+++ day05-targetresources/main.tf
@@ -1,22 +1,23 @@
-
-resource "aws_instance" "name" {
-    ami = "ami-07860a2d7eb515d9a"
-    instance_type = "t2.nano"
-    availability_zone = "us-east-1a"
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
     tags = {
-        Name = "dev"
+        Name = "my-vpc"
     }
-
+}
+resource "aws_instance" "name" {
+  ami = "ami-07860a2d7eb515d9a"
+    instance_type = "t3.micro"
+    tags = {
+      Name = "my-ec2-target-resources"
+    }
 }
 
 resource "aws_s3_bucket" "name" {
-    bucket = "hvdudvwgdvwugdcubbwdss"
+    bucket = "my-unique-bucket-name-29thoct-target-resources"
+    
+    tags = {
+        Name        = "my-s3-bucket-target-resources"
+        Environment = "Dev"
+    }
   
-
 }
-
-
-#teragte resource we can user to apply specific resource level only belwo command is the reference 
-#terraform apply -target=aws_s3_bucket.name
-
-#try skip resource 
